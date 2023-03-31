1ad0a0bed6ac7d18658bc4a519368daf461b8363
Juan Salazar <jjsc2003@live.com>
2023-03-31 07:53:30 -0600

    Table correctly connected to database

--- arco-system/src/App.js
+++ arco-system/src/App.js
@@ -1,47 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
+import Dashboard from "./Views/Dashboard";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.log(err));
-      // .then((data) => setData(data.message))
-      // .catch((err) => console.log(err));
-  }, []);
-
-  useEffect(() => {
-    fetch("/api/movies")
-      .then((res) => res.json())
-      .then((data) => setMovies(data.movies));
-  }, []);
-
   return (
-    <>
-    {console.log(data)}
-      {data ? data.map((dato, index) => {
-        return (
-          <>
-              <p>{dato.ID}</p>
-              <h5>{dato.F_NAME}</h5>
-          </>
-        )
-        
-      }) : "Loading..."}
-      {movies.map((movie, index) => {
-        return (
-          <>
-            <h1 key={index}>Nombre: {movie.name}</h1>
-            <h2 key={index}>Duración: {movie.duration}</h2>
-          </>
-        );
-      })}
-    </>
-  );
+    <Dashboard/>
+  )
 }
 
 export default App;
