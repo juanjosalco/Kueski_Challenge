f244ab083c2a6c0ebeb7cfbc3d355f2eb73199ce
BoJavs <javiereric1309@gmail.com>
2023-04-20 09:32:57 -0600

    Added put function for rectification and update in /user/:id

--- server/index.js
+++ server/index.js
@@ -40,6 +40,28 @@ app.get("/api/users/:id", (req, res) => {
   }
   );
 });
+app.put('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+  const { address_id, f_name, lname1, lname2, birth_date, nationality, state_born_in, occupation, curp, gender, phone_number, email, rfc, is_client, id_type, id_number } = req.body;
+
+  // Update user in the database
+  const userQuery = 'UPDATE USERS SET ADDRESS_ID = ?, F_NAME = ?, LNAME1 = ?, LNAME2 = ?, BIRTH_DATE = ?, NATIONALITY = ?, STATE_BORN_IN = ?, OCCUPATION = ?, CURP = ?, GENDER = ?, PHONE_NUMBER = ?, EMAIL = ?, RFC = ?, IS_CLIENT = ? WHERE ID = ?';
+  const userValues = [address_id, f_name, lname1, lname2, birth_date, nationality, state_born_in, occupation, curp, gender, phone_number, email, rfc, is_client, userId];
+  const userResult = await pool.query(userQuery, userValues);
+
+  // Update address in the database
+  const addressQuery = 'UPDATE ADDRESS SET COUNTRY = ?, STATE = ?, CITY = ?, NEIGHBORHOOD = ?, ZIP_CODE = ?, STREET = ?, EXT_NUMBER = ?, INT_NUMBER = ? WHERE ADDRESS_ID = ?';
+  const addressValues = [req.body.country, req.body.state, req.body.city, req.body.neighborhood, req.body.zip_code, req.body.street, req.body.ext_number, req.body.int_number, address_id];
+  const addressResult = await pool.query(addressQuery, addressValues);
+
+  // Update identification in the database
+  const idQuery = 'UPDATE IDENTIFICATIONS SET ID_TYPE = ?, ID_NUMBER = ? WHERE USER_ID = ?';
+  const idValues = [id_type, id_number, userId];
+  const idResult = await pool.query(idQuery, idValues);
+
+  res.status(200).send('User updated successfully');
+});
+  
 
 //arco
 app.get("/api/arco", (req, res) => {
@@ -62,6 +84,8 @@ app.get("/api/arco/:id", (req, res) => {
     res.send(result)
   });
 });
+  
+
 
 app.get("/api/pet", (req, res) => {
   fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
